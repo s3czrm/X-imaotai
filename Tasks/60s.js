5c9c6caa7d708e72e71515e0d2e9e1c92feb2887
onz3v <977732241@qq.com>
2023-06-12 21:30:20 +0800

    增加有道词典每日一言

--- Tasks/60s.js
+++ Tasks/60s.js
@@ -10,15 +10,19 @@
 30 8 * * * https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/60s.js, tag=每天60s读懂世界
 
 ******************************************/
+
 const $ = new Env('每天60s读懂世界');
 !(async () => {
-    await $.http.get('https://www.zhihu.com/api/v4/columns/c_1261258401923026944/items')
-        .then(({ body }) => {
+    await $.http.get({
+        url: 'https://www.zhihu.com/api/v4/columns/c_1261258401923026944/items',
+    })
+        .then(async ({ body }) => {
             const { data } = JSON.parse(body);
             const newItem = data[0];
             const { title, content } = newItem;
             const date = /\d{1,2}月\d{1,2}日/.exec(title)[0];
             const now = new Date();
+            const today = $.time('yyyy-MM-dd', now);
             const nowStr = `${now.getMonth() + 1}月${now.getDate()}日`;
             if (date !== nowStr) return $.logErr('还未更新今天的内容');
             let contentArr = content
@@ -32,7 +36,17 @@ const $ = new Env('每天60s读懂世界');
             const thumb = contentArr[0].match(/data-original='(.*?)'/)[1];
             const summary = contentArr.slice(1, 3).join('\n');
             const mainCon = contentArr.slice(3).map(item => item.split('<')[0]).join('\n');
-            $.msg(summary, '', mainCon, { "media-url": thumb })
+            const getCloseRemark = async () => {
+                const data = JSON.parse((await $.http.get(`https://dict.youdao.com/infoline?mode=publish&date=${today}&update=auto&apiversion=5.0`)).body);
+                const wordsList = data[today].map(item => {
+                    return { en: item.title, zh: item.summary }
+                })
+                const wordsIndex = Math.floor(Math.random() * wordsList.length);
+                return wordsList[wordsIndex];
+            };
+            const { zh, en } = await getCloseRemark();
+            const _content = mainCon + `\n【微语】\t${en}\t${zh}`;
+            $.msg(summary, '', _content, { "media-url": thumb })
         })
         .catch(err => $.logErr(err || '网络请求失败'))
 })().finally(() => $.done())
