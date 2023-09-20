b4b5b88041a2bd8c9cd7a6b46d51c4b2e85f38e0
yuheng <977732241@qq.com>
2023-09-20 16:55:57 +0800

    小爽音

---
+++ Scripts/xsy.js
@@ -0,0 +1,20 @@
+/******************************************
+ * @name 小爽音🚗
+ * @description 解锁会员、付费章节
+ * @statement 仅供学习交流|禁止用于商业用途|违者后果自负|
+ * @statement 转载请注明来源|欢迎分享|拒绝倒卖|倒卖🐕必死🐎
+ * @channel https://t.me/yqc_123/
+ * @feedback https://t.me/yqc_777/
+ * @download https://xsya1fggh.com?_s=GY4G
+ * @site https://h5.pornfm.net?code=GY4G
+ * @author 𝒀𝒖𝒉𝒆𝒏𝒈
+ * @update 20230920
+ * @version 1.0.0
+******************************************
+
+https://h5.pornfm.org/webapi/(book/(info|chapterList)|user/info) url script-response-body  https://raw.githubusercontent.com/Yuheng0101/X/main/Scripts/xsy/bodyRewrite.js
+
+https://h5.pornfm.org/webapi/book/chapterInfo url script-request-header https://raw.githubusercontent.com/Yuheng0101/X/main/Scripts/xsy/headersRewrite.js
+
+hostname = h5.pornfm.org
+******************************************/
